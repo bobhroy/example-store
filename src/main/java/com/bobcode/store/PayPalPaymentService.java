dfe14d8a714868456d66c842fcf0935c55ee4eea
bobhroy <bobhroy@gmail.com>
2025-08-23 14:42:21 -0700

    initial interfaces

---
+++ src/main/java/com/bobcode/store/PayPalPaymentService.java
@@ -0,0 +1,9 @@
+package com.bobcode.store;
+
+public class PayPalPaymentService implements PaymentService{
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("PAYPAL");
+        System.out.println("Amount: " + amount);
+    }
+}
---
+++ src/main/java/com/bobcode/store/PaymentService.java
@@ -0,0 +1,5 @@
+package com.bobcode.store;
+
+public interface PaymentService {
+    void processPayment(double amount);
+}
