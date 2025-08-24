2f10c2554d25711dc079fb1433fd2f041e6d26dc
bobhroy <bobhroy@gmail.com>
2025-08-24 08:08:50 -0700

    Config Bean using annotation

--- src/main/java/com/bobcode/store/OrderService.java
+++ src/main/java/com/bobcode/store/OrderService.java
@@ -1,11 +1,14 @@
 package com.bobcode.store;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
     private PaymentService paymentService;
 
-//    public OrderService(PaymentService paymentService) {
-//        this.paymentService = paymentService;
-//    }
+    public OrderService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
 
     public void placeOrder(){
         paymentService.processPayment(10);
