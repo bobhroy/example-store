85b21b6b65d744b1f35f63713f260279dc63a10e
bobhroy <bobhroy@gmail.com>
2025-08-24 09:38:24 -0700

    controlling Beans with yaml config

--- src/main/java/com/bobcode/store/AppConfig.java
+++ src/main/java/com/bobcode/store/AppConfig.java
@@ -1,18 +1,30 @@
 package com.bobcode.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+    @Value("${payment-gateway:stripe}")
+    private String paymentGateway;
+    
     //Always use noun (ex. stripe) instead of verbs (ex. getStripePaymentService)
     @Bean
     public PaymentService stripe() {
         return new StripePaymentService();
     }
-    
+
+    @Bean
+    public PaymentService paypal() {
+        return new PayPalPaymentService();
+    }
+
     @Bean
     public OrderService orderService() {
-        return new OrderService(stripe());
+        if(paymentGateway.equals("stripe")) {
+            return new OrderService(stripe());
+        }
+        return new OrderService(paypal());
     }
 }
