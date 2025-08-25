034588008677413bf695de0a8244dc58959d4632
bobhroy <bobhroy@gmail.com>
2025-08-24 20:19:34 -0700

    creating profile and tag table

---
+++ src/main/resources/db/migration/V4__add_profiles_table.sql
@@ -0,0 +1,9 @@
+create table profiles
+(
+    id             BIGINT PRIMARY KEY,
+    bio            TEXT,
+    phone_number   varchar(15),
+    date_of_birth  DATE,
+    loyalty_points INT UNSIGNED DEFAULT 0,
+    FOREIGN KEY (id) REFERENCES users(id)
+);
