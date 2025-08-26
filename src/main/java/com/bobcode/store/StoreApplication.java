52c4f36b0209b4acd04582a5976c4ab59f85208b
bobhroy <bobhroy@gmail.com>
2025-08-26 00:01:38 -0700

    minor clean up

--- src/main/java/com/bobcode/store/StoreApplication.java
+++ src/main/java/com/bobcode/store/StoreApplication.java
@@ -1,8 +1,5 @@
 package com.bobcode.store;
 
-import com.bobcode.store.entities.Address;
-import com.bobcode.store.entities.Profile;
-import com.bobcode.store.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,19 +8,6 @@ import org.springframework.context.ApplicationContext;
 public class StoreApplication {
 
 	public static void main(String[] args) {
-//        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-        var user = User.builder()
-                .name("John")
-                .password("password")
-                .email("john@testemail.com")
-                .build();
-
-        var profile = Profile.builder()
-                .bio("bio")
-                .build();
-        user.setProfile(profile);
-        profile.setUser(user);
-        System.out.println(user);
+        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
 	}
-
 }
