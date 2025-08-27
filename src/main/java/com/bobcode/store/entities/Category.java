2cdcc8de2a15bd9d8b0bce01b195daf77df26366
bobhroy <bobhroy@gmail.com>
2025-08-27 00:40:30 -0700

    Create a Product with a new Category

--- src/main/java/com/bobcode/store/entities/Category.java
+++ src/main/java/com/bobcode/store/entities/Category.java
@@ -2,26 +2,30 @@ package com.bobcode.store.entities;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.Set;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "categories")
 public class Category {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Byte id;
 
     @Column(name = "name")
     private String name;
 
-    @OneToMany
-    @JoinColumn(name = "category_id")
+    @OneToMany(mappedBy = "category")
     private Set<Product> products = new HashSet<>();
 
+    public Category(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/com/bobcode/store/services/UserService.java
+++ src/main/java/com/bobcode/store/services/UserService.java
@@ -1,14 +1,16 @@
 package com.bobcode.store.services;
 
 import com.bobcode.store.entities.Address;
+import com.bobcode.store.entities.Category;
+import com.bobcode.store.entities.Product;
 import com.bobcode.store.entities.User;
-import com.bobcode.store.repositories.AddressRepository;
-import com.bobcode.store.repositories.ProfileRepository;
-import com.bobcode.store.repositories.UserRepository;
+import com.bobcode.store.repositories.*;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @AllArgsConstructor
 @Service
@@ -17,6 +19,7 @@ public class UserService {
     private final ProfileRepository profileRepository;
     private final EntityManager entityManager;
     private final AddressRepository addressRepository;
+    private final ProductRepository productRepository;
 
     @Transactional
     public void showEntityState(){
@@ -73,4 +76,18 @@ public class UserService {
         user.removeAddress(address);
         userRepository.save(user);
     }
+
+    public void manageProducts(){
+        var category = new Category("Category 1");
+
+        var product = Product.builder()
+                .name("Product1")
+                .description("desc1")
+                .price(BigDecimal.valueOf(10.99))
+                .category(category)
+                .build();
+
+        productRepository.save(product);
+    }
+
 }
