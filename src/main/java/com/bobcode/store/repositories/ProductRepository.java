6da2ac226bd0d80d037c94e92e544e35c3dd4aa0
bobhroy <bobhroy@gmail.com>
2025-08-27 12:13:16 -0700

    fetching product using stored procedure

--- src/main/java/com/bobcode/store/repositories/ProductRepository.java
+++ src/main/java/com/bobcode/store/repositories/ProductRepository.java
@@ -7,6 +7,7 @@ import com.bobcode.store.entities.Product;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
@@ -51,8 +52,8 @@ public interface ProductRepository extends CrudRepository<Product, Long> {
 //    List<Product> findProducts(@Param("min") BigDecimal min, @Param("max") BigDecimal max);
 
     // JPQL
-    @Query("select p from Product p join p.category where p.price between :min and :max order by p.name")
-    List<Product> findProducts(@Param("min") BigDecimal min, @Param("max") BigDecimal max);
+    @Procedure("findProductsByPrice")
+    List<Product> findProducts(BigDecimal min, BigDecimal max);
 
     @Query("select count(*) from Product p where p.price between :min and :max")
     long countProducts(@Param("min") BigDecimal min, @Param("max") BigDecimal max);
